c8386286ec92721631482d026b63625cf2a26bb0
Calvin Metcalf <cmetcalf@appgeo.com>
2013-12-03 14:14:47 -0500

    first

---
+++ index.js
@@ -0,0 +1,54 @@
+var Rbush = require('rbush');
+var gbv = require('geojson-bounding-volume');
+
+function format(coords){
+  // transform from gbv which gives [[min1, min2, ...],[max1, max2, ...]]
+  //to rbush which wants [min1, min2, max1, max2]
+  return [coords[0][0],coords[0][1],coords[1][0],coords[1][1]];
+}
+
+function GeoBush(maxEntries){
+  if(!(this instanceof GeoBush)){
+    return new GeoBush(maxEntries);
+  }
+  this.rbush = new Rbush(maxEntries);
+  this.store = new WeakMap();
+}
+
+GeoBush.prototype.insert = function(feature){
+  if(Array.isArray(feature)||feature.type === 'FeatureCollection'){
+    this.bulkInsert(feature);
+  }
+  var coords = format(gbv(feature.geometry));
+  this.store.set(coords,feature);
+  this.store.set(feature,coords);
+  this.rbush.insert(coords);
+  return this;
+};
+
+GeoBush.prototype.load = function(geojson){
+  var array;
+  if(!Array.isArray(geojson)){
+    array = geojson.features;
+  }else{
+    array = geojson;
+  }
+  var coords = array.map(function(feature){
+    var coord = format(gbv(feature.geometry));
+    this.store.set(coord,feature);
+    this.store.set(feature,coord);
+  }, this);
+  this.rbush.load(coords);
+  return this;
+}
+
+GeoBush.prototype.search = function(bbox){
+  return this.rbush.search(bbox).map(function(coords){
+    return this.store.get(coords);
+  },this);
+}
+
+GeoBush.prototype.remove = function(item){
+  this.rbush.remove(this.store.get(item));
+  return this;
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+rbush.geojson
+====
+
+geojson wrapper for rbush, curently I need to actually test if it runs or not, also load symantics, requests harmony JavaScript features (WeakMap), because why not.  I may remove that around when I check if it actually runs. 
